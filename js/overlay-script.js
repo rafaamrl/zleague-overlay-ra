07d864b1e4628da28958472c899879c5ba756492
imdrk5 <79916095+imdrk5@users.noreply.github.com>
2021-05-20 23:55:04 +0100

    Recode scripts
    Recoded the overlay script to accept fortnite tournaments

--- js/overlay-script.js
+++ js/overlay-script.js
@@ -3,7 +3,7 @@ const urlParams = new URLSearchParams(queryString);
 if(urlParams.has('title') && urlParams.has('game') && urlParams.has('tournamentId') && urlParams.has('division') && urlParams.has('dm') && urlParams.has('shadow'))
 {
   title = urlParams.get('title');
-  document.title = "ZLeague Overlay | " + title;
+  document.title = "Z Overlay | " + title;
   game = urlParams.get('game');
   tournamentID = urlParams.get('tournamentId');
   division = urlParams.get('division');
@@ -30,6 +30,15 @@ shadows(shadow);
 
 /* SET TITLE */
 document.getElementById("title").innerHTML = title;
+/*  */
+
+/* SET URL */
+if(game == "fortnite") {
+  pre = "v2/fortnite/";
+}
+else {
+  pre = "";
+}
 /*  */
 
 /* REQUEST TOURNAMENT INFO */
@@ -59,17 +68,36 @@ requestInfo.send()
 /*  */
 
 /* REQUEST AND FILL TABLE WITH STANDINGS INFO */
-fillTable(tournamentID, division);
+var header = '<tr>';
+header += '<th id="h0" rowspan="2">#</th>';
+header += '<th id="h1" rowspan="2">NAME</th>';
+header += '<th id="h2" class="borderGames padding">GAMES</th>';
+header += '<th id="h3" rowspan="2">WINS</th>';
+header += '<th id="h4" colspan="2">BEST OF 2 GAMES</th>';
+if(game == "warzone") {
+  header += '<th id="h5" rowspan="2">KD</th>';
+}
+header += '<th id="h6" class="borderTotal padding">TOTAL</th>';
+header += '</tr>';
+header += '<tr>';
+header += '<th id="h7">PLAYED</th>';
+header += '<th id="h8" class="padding">KILLS</th>';
+header += '<th id="h9" class="padding">PLACEMENT</th>';
+header += '<th id="h10" class="borderPoints">POINTS</th>';
+header += '</tr>';
+document.getElementById("thead").innerHTML = header;
+
+fillTable(tournamentID, division, pre);
 var timerUpdate = setInterval(function() {
-  fillTable(tournamentID, division);
+  fillTable(tournamentID, division, pre);
 }, 60000);
 
 /**//**//**//**//**//* FUNCTIONS *//**//**//**//**//**/
 /* FILL TABLE FUNCTION */
-function fillTable(tournamentID, division)
+function fillTable(tournamentID, division, pre)
 {
   requestTable = new XMLHttpRequest()
-  requestTable.open('GET', 'https://zleague-api.herokuapp.com/tournament/' + tournamentID + '/standings/' + division, true)
+  requestTable.open('GET', 'https://zleague-api.herokuapp.com/' + pre + 'tournament/' + tournamentID + '/standings/' + division, true)
   requestTable.onload = function () {
     // Begin accessing JSON data here
     var table = JSON.parse(this.response)
@@ -79,16 +107,31 @@ function fillTable(tournamentID, division)
       lines = '';
       for(i=0;i<3;i++)
       {
-        lines += '<tr>'
-        lines += '<td>' + (i+1) + '</td>'
-        lines += '<td><div style="width: 115px; overflow: hidden;">' + table[i].name + '</div></td>'
-        lines += '<td>' + ("0" + table[i].total.games_played).slice(-2) + '</td>'
-        lines += '<td>' + ("0" + table[i].best.wins).slice(-2) + '</td>'
-        lines += '<td>' + ("00" + table[i].best.kills).slice(-2) + '</td>'
-        lines += '<td>' + ("0" + table[i].best.placement).slice(-2) + '</td>'
-        lines += '<td>' + Math.round((table[i].total.kills / table[i].total.deaths) * 100) / 100 + '</td>'
-        lines += '<td class="borderLeft">' + ("00" + table[i].best.combined_points).slice(-2) + '</td>'
-        lines += '</tr>'
+        if(game == "warzone")
+        {
+          lines += '<tr>'
+          lines += '<td>' + (i+1) + '</td>'
+          lines += '<td><div style="width: 115px; overflow: hidden;">' + table[i].name + '</div></td>'
+          lines += '<td>' + ("0" + table[i].total.games_played).slice(-2) + '</td>'
+          lines += '<td>' + ("0" + table[i].best.wins).slice(-2) + '</td>'
+          lines += '<td>' + ("00" + table[i].best.kills).slice(-2) + '</td>'
+          lines += '<td>' + ("0" + table[i].best.placement).slice(-2) + '</td>'
+          lines += '<td>' + Math.round((table[i].total.kills / table[i].total.deaths) * 100) / 100 + '</td>'
+          lines += '<td class="borderLeft">' + ("00" + table[i].best.combined_points).slice(-2) + '</td>'
+          lines += '</tr>'
+        }
+        else
+        {
+          lines += '<tr>'
+          lines += '<td>' + (i+1) + '</td>'
+          lines += '<td><div style="width: 207px; overflow: hidden;">' + table[i].name + '</div></td>'
+          lines += '<td>' + ("0" + table[i].numberOfGames).slice(-2) + '</td>'
+          lines += '<td>' + ("0" + table[i].totalWins).slice(-2) + '</td>'
+          lines += '<td>' + ("00" + table[i].bestKills).slice(-2) + '</td>'
+          lines += '<td>' + ("0" + table[i].bestPlacementPoints).slice(-2) + '</td>'
+          lines += '<td class="borderLeft">' + ("00" + table[i].totalPoints).slice(-2) + '</td>'
+          lines += '</tr>'
+        }
       }
       document.getElementById("tbody").innerHTML = lines;
     } else {
